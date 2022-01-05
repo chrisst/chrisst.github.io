ee025f09cd1b406dcc6b080ba08074639656b23e
Chris Stephens <chrisst@google.com>
2022-01-05 14:15:49 -0800

    adding pictures and title

--- js/main.js
+++ js/main.js
@@ -2,13 +2,8 @@ console.log("woo2")
 
 var countDownDate = new Date("Jan 5, 2022 13:37:25").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function () {
-
-  // Get today's date and time
   var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
   var distance = now - countDownDate;
 
   // Time calculations for days, hours, minutes and seconds
@@ -17,14 +12,7 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
 }, 1000);
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, send_from_directory
+import os
+from flask import Flask, send_file, send_from_directory
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
@@ -6,6 +7,14 @@ app = Flask(__name__, static_url_path='')
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
+
+@app.route('/img/<path:path>')
+def images(path):
+    """
+    Return a random image from the ones in the static/ directory
+    """
+    img_path = os.path.join("img", path)
+    return send_file(img_path, mimetype='image/gif')
 
 @app.route('/')
 def root():
