d044f1af7b53e02c5a0b53bf75b63dc7f993c327
Chris Stephens <chrisst@google.com>
2022-01-06 12:41:24 -0800

    Fix math

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ var x = setInterval(function () {
   var distance = now - countDownDate;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) % 2);
+  var days = 0;
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) % 2;
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
