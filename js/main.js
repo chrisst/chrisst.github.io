ab3952e7bd6916dddef7a646fff0452ce7804890
Chris Stephens <chrisst@google.com>
2022-01-11 23:47:52 -0800

    nobody will believe 3 hour update cadence any more

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log("woo3")
 
-var countDownDate = new Date("Jan 6, 2022 09:00:00").getTime();
+var countDownDate = new Date("Jan 11, 2022 23:45:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
@@ -8,7 +8,7 @@ var x = setInterval(function () {
 
   // Time calculations for days, hours, minutes and seconds
   var days = 0;
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) % 2;
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) % 4;
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
